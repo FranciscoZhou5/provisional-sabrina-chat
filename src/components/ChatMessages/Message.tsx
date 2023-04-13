b74c80193c464bd9927a5514dd5465ab4d7ad19b
FranciscoZhou5 <fg45874587@proton.me>
2023-04-12 22:37:19 -0300

    fix: use memo in Message component

---
+++ src/components/ChatMessages/Message.tsx
@@ -0,0 +1,38 @@
+import Image from "next/image";
+import MarkdownRenderer from "./MarkdownRenderer";
+import { ChatAgent } from "@/context/ChatContext";
+import classNames from "classnames";
+import { memo } from "react";
+
+interface IMessageProps {
+  content: string;
+  role: ChatAgent;
+}
+
+function Message({ content, role }: IMessageProps) {
+  return (
+    <div className={classNames("py-8 px-3 md:px-8", role === "user" ? "bg-gray-300 dark:bg-zinc-950" : "")}>
+      <div className="max-w-[800px] mx-auto flex gap-4 md:gap-6 lg:gap-8 relative">
+        <div className="h-full flex items-start py-2">
+          <div className="w-10 h-10 relative">
+            <Image
+              src={role === "user" ? "/user.gif" : "/chisato.jpg"}
+              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+              alt="User avatar"
+              fill
+              className="object-cover rounded-sm"
+            />
+          </div>
+        </div>
+
+        <div className="flex justify-center flex-col w-[84%] text-sm md:text-base">
+          <MarkdownRenderer content={content} />
+        </div>
+
+        {/* <div className="absolute right-4 top-0"> ola</div> */}
+      </div>
+    </div>
+  );
+}
+
+export default memo(Message);
--- src/components/ChatMessages/index.tsx
+++ src/components/ChatMessages/index.tsx
@@ -1,48 +1,16 @@
-import { memo } from "react";
-import Image from "next/image";
+import { useChatContext } from "@/context/ChatContext";
+import Message from "./Message";
 
-import MarkdownRenderer from "./MarkdownRenderer";
-
-import { ChatAgent, useChatContext } from "@/context/ChatContext";
-import classNames from "classnames";
-
-interface IMessageProps {
-  content: string;
-  role: ChatAgent;
-}
-
-function ChatMessages() {
+export default function ChatMessages() {
   const { messages } = useChatContext();
 
   return (
     <>
       {messages.map(({ content, role }) => (
-        <div className={classNames("py-8 px-3 md:px-8", role === "user" ? "bg-gray-300 dark:bg-zinc-950" : "")} key={Math.random()}>
-          <div className="max-w-[800px] mx-auto flex gap-4 md:gap-6 lg:gap-8 relative">
-            <div className="h-full flex items-start py-2">
-              <div className="w-10 h-10 relative">
-                <Image
-                  src={role === "user" ? "/user.gif" : "/chisato.jpg"}
-                  sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                  alt="User avatar"
-                  fill
-                  className="object-cover rounded-sm"
-                />
-              </div>
-            </div>
-
-            <div className="flex justify-center flex-col w-[84%] text-sm md:text-base">
-              <MarkdownRenderer content={content} />
-            </div>
-
-            {/* <div className="absolute right-4 top-0"> ola</div> */}
-          </div>
-        </div>
+        <Message key={Math.random()} content={content} role={role} />
       ))}
 
       <div className="h-[50vh]"></div>
     </>
   );
 }
-
-export default memo(ChatMessages);

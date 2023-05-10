6c65753ac374a102a307db07983ec11a27002cbf
Francisco Zhou <113115839+FranciscoZhou5@users.noreply.github.com>
2023-05-10 07:59:48 -0300

    Update kakas8318294kak.tsx

--- src/pages/kakas8318294kak.tsx
+++ src/pages/kakas8318294kak.tsx
@@ -18,6 +18,8 @@ interface IProps {
 function PromptsList({ prompts, totalPages, currentPage }: IProps) {
   const router = useRouter();
 
+  console.log(prompts)
+
   // if (true) {
   //   return <></>;
   // }
